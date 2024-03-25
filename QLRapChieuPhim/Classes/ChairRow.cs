b424d261a3d7155b2fe8fdb4274fd2d33128eb42
longhms <130201421+longhms@users.noreply.github.com>
2024-03-25 23:14:09 +0700

    mimi

---
+++ QLRapChieuPhim/Classes/ChairRow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLRapChieuPhim.Classes
+{
+    internal class ChairRow
+    {
+        public ChairRow() { }
+
+        int idRow {  get; set; }
+        string nameRow { get; set; }
+
+        public ChairRow(int idRow, string nameRow)
+        {
+            this.idRow = idRow;
+            this.nameRow = nameRow;
+        }
+    }
+}
