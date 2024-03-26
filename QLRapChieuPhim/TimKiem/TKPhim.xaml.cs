d99981c7214d5544b2487556684a423973374773
Tdthang2003 <21013042@st.phenikaa-uni.edu.vn>
2024-03-26 20:52:17 +0700

    find the loai

--- QLRapChieuPhim/TimKiem/TKPhim.xaml.cs
+++ QLRapChieuPhim/TimKiem/TKPhim.xaml.cs
@@ -30,21 +30,23 @@ namespace QLRapChieuPhim.TimKiem
 
         void LoadData()
         {
+
             string sql = @"SELECT 
-                        P.maPhim, 
-                        P.tenPhim, 
-                        Q.tenQGSanXuat AS TenQuocGia, 
-                        P.maHangSX, 
-                        P.daoDien, 
-                        T.tenTheLoai AS TenTheLoai, 
-                        P.ngayKhoiChieu, 
-                        P.ngayKetThuc, 
-                        P.nuDVC, 
-                        P.namDVC, 
-                        P.noiDungC 
-                    FROM tblPhim AS P
-                    LEFT JOIN tblTheLoai AS T ON P.maTheLoai = T.maTheLoai
-                    LEFT JOIN tblQGSanXuat AS Q ON P.maQGSanXuat = Q.maQGSanXuat";
+                    P.maPhim, 
+                    P.tenPhim, 
+                    Q.tenQGSanXuat AS TenQuocGia, 
+                    H.tenHangSX AS TenHangSX, 
+                    P.daoDien, 
+                    T.tenTheLoai AS TenTheLoai, 
+                    P.ngayKhoiChieu, 
+                    P.ngayKetThuc, 
+                    P.nuDVC, 
+                    P.namDVC, 
+                    P.noiDungC 
+                FROM tblPhim AS P
+                LEFT JOIN tblTheLoai AS T ON P.maTheLoai = T.maTheLoai
+                LEFT JOIN tblQGsanXuat AS Q ON P.maQGSanXuat = Q.maQGSanXuat
+                LEFT JOIN tblHangSX AS H ON P.maHangSX = H.maHangSX";
 
             DataTable dtPhim = dtBase.ReadData(sql);
             dtgTKPhim.ItemsSource = dtPhim.AsDataView();
@@ -61,31 +63,31 @@ namespace QLRapChieuPhim.TimKiem
             dtgTKPhim.Columns[10].Header = "Nội dung chính";
         }
 
-        private void btnTim_Click(object sender, RoutedEventArgs e)
+        private void txtFindAll_TextChanged(object sender, TextChangedEventArgs e)
         {
-
             string sql = @"SELECT P.maPhim, 
-                          P.tenPhim, 
-                          Q.tenQGSanXuat AS TenQuocGia, 
-                          P.maHangSX, 
-                          P.daoDien, 
-                          T.tenTheLoai AS TenTheLoai, 
-                          P.ngayKhoiChieu, 
-                          P.ngayKetThuc, 
-                          P.nuDVC, 
-                          P.namDVC, 
-                          P.noiDungC 
-                   FROM tblPhim AS P
-                   LEFT JOIN tblTheLoai AS T ON P.maTheLoai = T.maTheLoai
-                   LEFT JOIN tblQGSanXuat AS Q ON P.maQGSanXuat = Q.maQGSanXuat
-                   WHERE 1=1";
+          P.tenPhim, 
+          Q.tenQGSanXuat AS TenQuocGia, 
+          H.tenHangSX AS TenHangSX, 
+          P.daoDien, 
+          T.tenTheLoai AS TenTheLoai, 
+          P.ngayKhoiChieu, 
+          P.ngayKetThuc, 
+          P.nuDVC, 
+          P.namDVC, 
+          P.noiDungC 
+           FROM tblPhim AS P
+           LEFT JOIN tblTheLoai AS T ON P.maTheLoai = T.maTheLoai
+           LEFT JOIN tblQGsanXuat AS Q ON P.maQGSanXuat = Q.maQGsanXuat
+           LEFT JOIN tblHangSX AS H ON P.maHangSX = H.maHangSX
+           WHERE 1=1";
 
             if (!string.IsNullOrEmpty(txtFindAll.Text.Trim()))
             {
                 sql += " AND (P.maPhim LIKE '%" + txtFindAll.Text + "%' OR ";
                 sql += "P.tenPhim LIKE '%" + txtFindAll.Text + "%' OR ";
                 sql += "Q.tenQGSanXuat LIKE '%" + txtFindAll.Text + "%' OR ";
-                sql += "P.maHangSX LIKE '%" + txtFindAll.Text + "%' OR ";
+                sql += "H.tenHangSX LIKE '%" + txtFindAll.Text + "%' OR ";
                 sql += "P.daoDien LIKE '%" + txtFindAll.Text + "%' OR ";
                 sql += "T.tenTheLoai LIKE '%" + txtFindAll.Text + "%' OR ";
                 sql += "P.ngayKhoiChieu LIKE '%" + txtFindAll.Text + "%' OR ";
@@ -96,11 +98,92 @@ namespace QLRapChieuPhim.TimKiem
 
             DataTable dtTimKiem = dtBase.ReadData(sql);
             dtgTKPhim.ItemsSource = dtTimKiem.AsDataView();
+            dtgTKPhim.Columns[0].Header = "Mã Phim";
+            dtgTKPhim.Columns[1].Header = "Tên Phim";
+            dtgTKPhim.Columns[2].Header = "Quốc gia sản xuất";
+            dtgTKPhim.Columns[3].Header = "Hãng sản xuất";
+            dtgTKPhim.Columns[4].Header = "Đạo diễn";
+            dtgTKPhim.Columns[5].Header = "Thể loại";
+            dtgTKPhim.Columns[6].Header = "Ngày khởi chiếu";
+            dtgTKPhim.Columns[7].Header = "Ngày kết thúc ";
+            dtgTKPhim.Columns[8].Header = "Nữ diễn viên chính";
+            dtgTKPhim.Columns[9].Header = "Nam diễn viên chính";
+            dtgTKPhim.Columns[10].Header = "Nội dung chính";
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            DataTable dtTheloai = dtBase.ReadData("Select * from tblTheLoai");
+            cboTheloai.ItemsSource = dtTheloai.AsDataView();
+            cboTheloai.DisplayMemberPath = "tenTheLoai";
+            cboTheloai.SelectedValuePath = "maTheLoai";
             LoadData();
+            LoadDataAndSetupComboBox();
         }
+
+        private void cboTheloai_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cboTheloai.SelectedItem != null)
+            {
+                DataRowView selectedRow = cboTheloai.SelectedItem as DataRowView;
+                string selectedTheLoai = selectedRow["tenTheLoai"].ToString();
+                SearchByTheLoai(selectedTheLoai);
+            }
+        }
+
+        private void SearchByTheLoai(string tenTheLoai)
+        {
+            string sql = @"SELECT P.maPhim, 
+                  P.tenPhim, 
+                  Q.tenQGSanXuat AS TenQuocGia, 
+                  H.tenHangSX AS TenHangSX, 
+                  P.daoDien, 
+                  T.tenTheLoai AS TenTheLoai, 
+                  P.ngayKhoiChieu, 
+                  P.ngayKetThuc, 
+                  P.nuDVC, 
+                  P.namDVC, 
+                  P.noiDungC 
+           FROM tblPhim AS P
+           LEFT JOIN tblTheLoai AS T ON P.maTheLoai = T.maTheLoai
+           LEFT JOIN tblQGsanXuat AS Q ON P.maQGSanXuat = Q.maQGsanXuat
+           LEFT JOIN tblHangSX AS H ON P.maHangSX = H.maHangSX
+           WHERE 1=1";
+
+            if (!string.IsNullOrEmpty(tenTheLoai))
+            {
+                sql += " AND tenTheLoai LIKE '%" + tenTheLoai + "%'";
+            }
+
+            DataTable dtTimKiem = dtBase.ReadData(sql);
+            dtgTKPhim.ItemsSource = dtTimKiem.AsDataView();
+            dtgTKPhim.Columns[0].Header = "Mã Phim";
+            dtgTKPhim.Columns[1].Header = "Tên Phim";
+            dtgTKPhim.Columns[2].Header = "Quốc gia sản xuất";
+            dtgTKPhim.Columns[3].Header = "Hãng sản xuất";
+            dtgTKPhim.Columns[4].Header = "Đạo diễn";
+            dtgTKPhim.Columns[5].Header = "Thể loại";
+            dtgTKPhim.Columns[6].Header = "Ngày khởi chiếu";
+            dtgTKPhim.Columns[7].Header = "Ngày kết thúc ";
+            dtgTKPhim.Columns[8].Header = "Nữ diễn viên chính";
+            dtgTKPhim.Columns[9].Header = "Nam diễn viên chính";
+            dtgTKPhim.Columns[10].Header = "Nội dung chính";
+        }
+
+        private void LoadDataAndSetupComboBox()
+        {
+            
+            LoadTheLoaiComboBox();
+            cboTheloai.SelectionChanged += cboTheloai_SelectionChanged;
+        }
+
+        private void LoadTheLoaiComboBox()
+        {
+            DataTable dtTheLoai = dtBase.ReadData("SELECT * FROM tblTheLoai");
+            cboTheloai.DisplayMemberPath = "tenTheLoai";
+            cboTheloai.ItemsSource = dtTheLoai.DefaultView;
+        }
+
+        
     }
 }
