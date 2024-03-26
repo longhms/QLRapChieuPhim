2cb4e49a6e9885a4d21e60df18056c09e94b3d84
Tdthang2003 <21013042@st.phenikaa-uni.edu.vn>
2024-03-26 16:04:34 +0700

    complete Find movie

--- QLRapChieuPhim/TimKiem/TKPhim.xaml.cs
+++ QLRapChieuPhim/TimKiem/TKPhim.xaml.cs
@@ -30,7 +30,24 @@ namespace QLRapChieuPhim.TimKiem
 
         void LoadData()
         {
-            DataTable dtPhim = dtBase.ReadData("Select maPhim,tenPhim,maQGSanXuat,maHangSX,daoDien,maTheloai,ngayKhoiChieu,ngayKetThuc,nuDVC,namDVC,noiDungC from tblPhim");
+
+            string sql = @"SELECT 
+                        P.maPhim, 
+                        P.tenPhim, 
+                        Q.tenQGSanXuat AS TenQuocGia, 
+                        P.maHangSX, 
+                        P.daoDien, 
+                        T.tenTheLoai AS TenTheLoai, 
+                        P.ngayKhoiChieu, 
+                        P.ngayKetThuc, 
+                        P.nuDVC, 
+                        P.namDVC, 
+                        P.noiDungC 
+                    FROM tblPhim AS P
+                    LEFT JOIN tblTheLoai AS T ON P.maTheLoai = T.maTheLoai
+                    LEFT JOIN tblQGSanXuat AS Q ON P.maQGSanXuat = Q.maQGSanXuat";
+
+            DataTable dtPhim = dtBase.ReadData(sql);
             dtgTKPhim.ItemsSource = dtPhim.AsDataView();
             dtgTKPhim.Columns[0].Header = "Mã Phim";
             dtgTKPhim.Columns[1].Header = "Tên Phim";
@@ -48,26 +65,42 @@ namespace QLRapChieuPhim.TimKiem
         private void btnTim_Click(object sender, RoutedEventArgs e)
         {
 
-            string sql = "SELECT * FROM tblPhim WHERE 1=1";
-
+            string sql = @"SELECT P.maPhim, 
+                          P.tenPhim, 
+                          Q.tenQGSanXuat AS TenQuocGia, 
+                          P.maHangSX, 
+                          P.daoDien, 
+                          T.tenTheLoai AS TenTheLoai, 
+                          P.ngayKhoiChieu, 
+                          P.ngayKetThuc, 
+                          P.nuDVC, 
+                          P.namDVC, 
+                          P.noiDungC 
+                   FROM tblPhim AS P
+                   LEFT JOIN tblTheLoai AS T ON P.maTheLoai = T.maTheLoai
+                   LEFT JOIN tblQGSanXuat AS Q ON P.maQGSanXuat = Q.maQGSanXuat
+                   WHERE 1=1";
 
             if (!string.IsNullOrEmpty(txtFindAll.Text.Trim()))
             {
-                sql += " AND (maPhim LIKE '%" + txtFindAll.Text + "%' OR ";
-                sql += "tenPhim LIKE '%" + txtFindAll.Text + "%' OR ";
-                sql += "maQGSanXuat LIKE '%" + txtFindAll.Text + "%' OR ";
-                sql += "maHangSX LIKE '%" + txtFindAll.Text + "%' OR ";
-                sql += "daoDien LIKE '%" + txtFindAll.Text + "%' OR ";
-                sql += "maTheLoai LIKE '%" + txtFindAll.Text + "%' OR ";
-                sql += "ngayKhoiChieu LIKE '%" + txtFindAll.Text + "%' OR ";
-                sql += "ngayKetThuc LIKE '%" + txtFindAll.Text + "%' OR ";
-                sql += "nuDVC LIKE '%" + txtFindAll.Text + "%' OR ";
-                sql += "namDVC LIKE '%" + txtFindAll.Text + "%')";
-
+                sql += " AND (P.maPhim LIKE '%" + txtFindAll.Text + "%' OR ";
+                sql += "P.tenPhim LIKE '%" + txtFindAll.Text + "%' OR ";
+                sql += "Q.tenQGSanXuat LIKE '%" + txtFindAll.Text + "%' OR ";
+                sql += "P.maHangSX LIKE '%" + txtFindAll.Text + "%' OR ";
+                sql += "P.daoDien LIKE '%" + txtFindAll.Text + "%' OR ";
+                sql += "T.tenTheLoai LIKE '%" + txtFindAll.Text + "%' OR ";
+                sql += "P.ngayKhoiChieu LIKE '%" + txtFindAll.Text + "%' OR ";
+                sql += "P.ngayKetThuc LIKE '%" + txtFindAll.Text + "%' OR ";
+                sql += "P.nuDVC LIKE '%" + txtFindAll.Text + "%' OR ";
+                sql += "P.namDVC LIKE '%" + txtFindAll.Text + "%')";
             }
 
             DataTable dtTimKiem = dtBase.ReadData(sql);
             dtgTKPhim.ItemsSource = dtTimKiem.AsDataView();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
             LoadData();
         }
     }
