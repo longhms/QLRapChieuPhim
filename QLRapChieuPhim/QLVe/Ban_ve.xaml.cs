988a9a90661c935d55e24f300df0af3388be57aa
21012313 <trandattsh@gmail.com>
2024-03-27 08:51:37 +0700

    Add ban ve

---
+++ QLRapChieuPhim/QLVe/Ban_ve.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QLRapChieuPhim.QLVe
+{
+    /// <summary>
+    /// Interaction logic for Ban_ve.xaml
+    /// </summary>
+    public partial class Ban_ve : Window
+    {
+        public Ban_ve()
+        {
+            InitializeComponent();
+        }
+    }
+}
