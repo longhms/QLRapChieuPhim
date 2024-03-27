35a30b71860d60168a7c3698f70d3533804b68ed
21012313 <trandattsh@gmail.com>
2024-03-27 14:57:41 +0700

    all

--- QLRapChieuPhim/QLVe/Ban_ve.xaml.cs
+++ QLRapChieuPhim/QLVe/Ban_ve.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -44,5 +45,7 @@ namespace QLRapChieuPhim.QLVe
                 this.Close();
             }
         }
+
+       
     }
 }
